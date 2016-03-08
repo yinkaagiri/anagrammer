b9b9f1344ac3499e80cf2c1f8258f3b1858ef0a8
mattscilipoti <matt.scilipoti@ga.co>
2016-03-08 07:53:54 -0500

    solution

--- anagram.rb
+++ anagram.rb
@@ -1,9 +1,11 @@
 # Check if two words are anagrams of each other
-def anagram?(w1, w2)
-
+def anagram?(word1, word2)
+  word1.chars.sort == word2.chars.sort
 end
 
-# Check each anagrams in the word list
+# Check each word in the word list
 def find_anagrams(base_word, word_list)
-
+  word_list.select do |word|
+    anagram?(base_word, word)
+  end
 end
