3fa95ddf0fb50e4cf9254df9899e24bca17be2df
Fatai Agiri <yinka.fatai@gmail.com>
2016-03-08 09:17:01 -0500

    initial commit

--- anagram.rb
+++ anagram.rb
@@ -1,3 +1,12 @@
 # Check each anagram in the word list
+class Anagram
+  attr_accessor(:base_word, :word_list)
+
+  def initilize (base_word, word_list)
+    @base_word = base_word
+    @word_list = word_list
 def find_anagrams(base_word, word_list)
+  base_word.split("")
+end
+end
 end
