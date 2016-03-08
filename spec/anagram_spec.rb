54ad0a6c994e316cabeda67eb029326e0ec0f8b1
mattscilipoti <matt.scilipoti@ga.co>
2016-03-08 07:52:29 -0500

    rspec: correct be_true matchers, Gemfile

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -2,15 +2,15 @@ require_relative '../anagram'
 
 describe "#anagram?" do
   it "returns true for 2 words that are anagrams" do
-    expect(anagram?("steven", "events")).to be_true
+    expect(anagram?("steven", "events")).to be true
   end
 
   it "returns false for 2 words that are not anagrams" do
-    expect(anagram?("drew", "granger")).to be_false
+    expect(anagram?("drew", "granger")).to be false
   end
 
   it "returns false for 2 words that have the same letters but are different lengths" do
-    expect(anagram?("kevin", "keevin")).to be_false
+    expect(anagram?("kevin", "keevin")).to be false
   end
 
 end
@@ -24,8 +24,7 @@ describe "#find_anagrams" do
     expect(find_anagrams("listen", %w(enlists google inlets banana))).to eq(["inlets"])
   end
 
-    it "returns multiple matches" do
-    expect(find_anagrams("listen", %w(enlists google inlets banana enlist))).to include("enlist")
-    expect(find_anagrams("listen", %w(enlists google inlets banana enlist))).to include("inlets")
+  it "returns multiple matches" do
+    expect(find_anagrams("listen", %w(enlists google inlets banana enlist))).to contain_exactly("enlist", "inlets")
   end
 end
