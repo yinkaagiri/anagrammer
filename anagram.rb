83a4cd6224f9406090fff56d0fcdabb05235f4a1
mattscilipoti <matt.scilipoti@ga.co>
2016-03-08 07:46:15 -0500

    add hint: anagram? method with specs

--- anagram.rb
+++ anagram.rb
@@ -1,3 +1,9 @@
-# Check each anagram in the word list
+# Check if two words are anagrams of each other
+def anagram?(w1, w2)
+
+end
+
+# Check each anagrams in the word list
 def find_anagrams(base_word, word_list)
+
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -1,5 +1,20 @@
 require_relative '../anagram'
 
+describe "#anagram?" do
+  it "returns true for 2 words that are anagrams" do
+    expect(anagram?("steven", "events")).to be_true
+  end
+
+  it "returns false for 2 words that are not anagrams" do
+    expect(anagram?("drew", "granger")).to be_false
+  end
+
+  it "returns false for 2 words that have the same letters but are different lengths" do
+    expect(anagram?("kevin", "keevin")).to be_false
+  end
+
+end
+
 describe "#find_anagrams" do
   it "doesn't return any for no matches" do
     expect(find_anagrams("bing", %w(enlists google inlets banana))).to eq([])
